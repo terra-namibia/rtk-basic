05d50c600b250d09229cafb3135750bc4c12b49e
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-03 09:52:41 +0900

    add action and dispatch from TaskItem component

--- src/features/task/TaskItem.js
+++ src/features/task/TaskItem.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { completeTask, deleteTask } from "./taskSlice";
 
 function TaskItem({ task }) {
-  return <div>{task.id}</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        onClick={() => dispatch(completeTask(task))}
+        defaultChecked={task.completed}
+      />
+      <span> {task.title} </span>
+      <button onClick={() => dispatch(deleteTask(task))}>DELETE</button>
+    </div>
+  );
 }
 
 export default TaskItem;
